b0e2900f96cab31ef1941a95132a47212044cb61
Flavio <flavio.origgi@outlook.it>
2021-06-07 13:24:38 +0200

    sto eseguendo "pari e dispari". generazione ok

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// Palidroma: 
+//   -Chiedere all’utente di inserire una parola; 
+//   -Creare una funzione per capire se la parola inserita è palindroma.
+
+// var parola = prompt('inserisci una parola');
+// console.log(parola);
+
+
+
+// function {
+  
+// }
+
+
+
+
+// Pari e Dispari:
+//   -L’utente sceglie pari o dispari e inserisce un numero da 1 a 5; 
+//   -Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione);
+//   -Sommiamo i due numeri, stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione);
+//   -Dichiariamo chi ha vinto.
+
+// scelta utente di tipologia e numero 
+var scelta = prompt('Scegli pari o dispari');
+var numeroUtente = prompt('Scrivi un numero da 1 a 5');
+console.log(scelta);
+console.log(numeroUtente);
+
+// generazione numero casuale del pc
+var numeroPc = randomNumber(1,5);
+
+function randomNumber(min, max){
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+console.log(numeroPc);
