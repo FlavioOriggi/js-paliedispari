8dfa34d2bb8bcdc48fd4dd8fccfc7e98d85c5a2b
Flavio <flavio.origgi@outlook.it>
2021-06-07 15:11:02 +0200

    esercizio divisio su due file script.js

---
+++ js/scriptPalidroma.js
@@ -0,0 +1,12 @@
+// Palidroma: 
+//   -Chiedere all’utente di inserire una parola; 
+//   -Creare una funzione per capire se la parola inserita è palindroma.
+
+// var parola = prompt('inserisci una parola');
+// console.log(parola);
+
+
+
+// function {
+  
+// }
