090b76ce25536df2e551db214b656151322775fb
Flavio <flavio.origgi@outlook.it>
2021-06-07 16:46:26 +0200

    palindroma, non riesco a capire come invertire senza .reverse()

--- js/scriptPalidroma.js
+++ js/scriptPalidroma.js
@@ -1,12 +1,27 @@
-// Palidroma: 
+Palidroma: 
 //   -Chiedere all’utente di inserire una parola; 
 //   -Creare una funzione per capire se la parola inserita è palindroma.
 
-// var parola = prompt('inserisci una parola');
-// console.log(parola);
+
+// Chiedere all’utente di inserire una parola; 
+var parola = prompt('inserisci una parola che pensi possa essere palidroma');
+
+var arrayUtente = [];
+arrayUtente.push(parola);
+
+console.log(arrayUtente);
 
 
+function invertiEstampaStringa(parola){
+    for (var i=parola.length-1; i>=0; i--)document.write(parola.charAt(i));
+    console.log("");
+}
 
-// function {
+var risultato = invertiEstampaStringa(parola);
+console.log(risultato);
+
+    
   
-// }
+
+   
+
